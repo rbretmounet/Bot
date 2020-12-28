886c3c216c7648e11c9c22c1f4b932f52a5dba9e
rbretmounet <rbretmounet@gmail.com>
2020-12-28 00:57:20 -0800

    Added Comments

--- upload.py
+++ upload.py
@@ -14,22 +14,25 @@ def isElementExist(browser, element):
     except:
         flag = False
         return flag
+
 def upload_video(browser):
     path = str(pathlib.Path(__file__).parent.absolute())
-
+    # Close Verification Window:
     WebDriverWait(browser, 100).until(
         EC.presence_of_all_elements_located((By.CLASS_NAME, 'verify-bar-close'))
     )
     verification_button = browser.find_element_by_class_name('verify-bar-close')
     verification_button.click()
 
+    # Open Upload Menu
     WebDriverWait(browser, 100).until(
         EC.presence_of_all_elements_located((By.CLASS_NAME, 'jsx-2482409767'))
     )
 
     upload_button = browser.find_element_by_class_name('jsx-2482409767')
     upload_button.click()
-
+    
+    # Uploads Video
     WebDriverWait(browser, 100).until(
         EC.presence_of_all_elements_located((By.CLASS_NAME, 'upload-btn-input'))
     )
@@ -39,15 +42,16 @@ def upload_video(browser):
     WebDriverWait(browser, 100).until_not(
         EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.button.disabled.jsx-352266594'))
     )
-    print("here")
 
+    # Adds video description
     video_title_input = browser.find_element_by_class_name("public-DraftStyleDefault-block")
     myFile = open(path + "\\downloaded\\desc.txt", "rt")
     contents = str(myFile.read())
 
     myFile.close()
     video_title_input.send_keys(contents)
-
+    
+    # Posts video
     submit_button = browser.find_element_by_class_name('btn-post')
     submit_button.click()
 
@@ -72,4 +76,4 @@ if isExistLogin:
 else:
     print('You are already logged in')
     upload_video(browser)
-browser.exit()
+    browser.exit()
